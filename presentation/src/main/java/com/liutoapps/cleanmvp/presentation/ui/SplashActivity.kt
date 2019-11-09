9f290ed9f9ada16b918fa52490a3c2d4f1113978
Steven <sluijts@gmail.com>
2019-11-09 22:07:39 +0100

    Added a SplashScreen

---
+++ presentation/src/main/java/com/liutoapps/cleanmvp/presentation/ui/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.liutoapps.cleanmvp.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.liutoapps.cleanmvp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
